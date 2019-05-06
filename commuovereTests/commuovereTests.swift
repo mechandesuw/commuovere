a84f931f308ed6c983750b95ac2f5e5fc8708812
mechandesuw <w.mick_ovo@ezweb.ne.jp>
2019-05-06 15:28:28 +0900

    Initial Commit

---
+++ commuovereTests/commuovereTests.swift
@@ -0,0 +1,34 @@
+//
+//  commuovereTests.swift
+//  commuovereTests
+//
+//  Created by 山谷美咲生 on 2019/04/27.
+//  Copyright © 2019 山谷美咲生. All rights reserved.
+//
+
+import XCTest
+@testable import commuovere
+
+class commuovereTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
